8962d58bf90035d90db0375fd878c72a073b6565
vander-el <powerelmusic@gmail.com>
2025-05-13 23:11:58 -0400

    Update subir_a_firebase.py

--- subir_a_firebase.py
+++ subir_a_firebase.py
@@ -1,16 +1,1 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-from datetime import datetime
 
-cred = credentials.Certificate("credenciales.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-def subir_datos(artista, cancion, emisora):
-    data = {
-        "artista": artista,
-        "cancion": cancion,
-        "emisora": emisora,
-        "fecha": datetime.now().isoformat()
-    }
-    db.collection("monitoreo").add(data)
